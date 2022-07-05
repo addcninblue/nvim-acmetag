94cdac4eb0b49ca1a02b79b28b2c1739cc73e291
Addison Chan <addison95132@gmail.com>
2022-07-04 22:57:36 -0700

    New version of acmetag: inspired by c9x.me Edit editor

--- fnl/acmetag.fnl
+++ fnl/acmetag.fnl
@@ -1,38 +1,46 @@
 (local f vim.fn)
 (local api vim.api)
+(local cmd vim.cmd)
+(local Job (require "plenary.job"))
 
-(lambda open-split [direction]
-  "Opens bottom split terminal at 20% height."
-  (let [bufheight (f.floor (/ (f.winheight 0) 5))]
-    (vim.cmd (.. direction " " bufheight "split"))
+; TODO: add support for `<|>`
+; TODO: think about support for opening .tagbar in multiple editor instances?
+
+(var tagbufnr nil)
+(var pids {})
+(var ns (api.nvim_create_namespace "acmetag"))
+
+(fn kill-pid [pid killcode?]
+  (io.popen (.. "kill -" (or killcode? 9) " " pid))) ;; NOTE: use nicer way to terminate maybe?
+
+(lambda kill-all-running-processes []
+  (each [_ pid (pairs pids)]
+    (kill-pid pid)))
+
+(api.nvim_create_autocmd "VimLeavePre" {:callback kill-all-running-processes})
+
+; (lambda open-split [direction]
+;   "Opens bottom split terminal at 20% height."
+;   (let [bufheight (f.floor (/ (f.winheight 0) 5))]
+;     (vim.cmd (.. direction " " bufheight "split"))
+;     (local bufnr (api.nvim_create_buf false false))
+;     (api.nvim_set_current_buf bufnr)
+;     bufnr))
+
+(lambda open-vert-split [direction]
+  "Opens vertical split terminal at 33% width"
+  (let [bufheight (f.floor (/ (f.winwidth 0) 3))]
+    (vim.cmd (.. direction " " bufheight "vsplit"))
     (local bufnr (api.nvim_create_buf false false))
     (api.nvim_set_current_buf bufnr)
     bufnr))
 
-(lambda run [letter]
-  (let [command (-> letter
-                    (f.getreg)
-                    (string.match "^%s*(.-)%s*$")) ; Delete whitespace
-        bufheight (f.floor (/ (f.winheight 0) 5))]
-    (if (= command "")
-      (print (.. "nothing bound to register '" letter "' !"))
-      (do
-        (open-split "belowright")
-        (f.termopen command)
-        (api.nvim_command "startinsert")))))
-
-(local letters [ "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ])
-
-(lambda display-registers []
-  (let [bufnr (open-split "topleft")]
-    (api.nvim_buf_set_lines bufnr 0 -1 true
-                            (icollect [_ letter (ipairs letters)]
-                                      (let [reg (f.getreg letter 1)]
-                                        (if (not (string.find reg "\n"))
-                                          (.. letter ": " reg)))))
-    (set vim.bo.bufhidden "hide")
-    (set vim.bo.buflisted false)
-    (set vim.bo.buftype "nofile")))
+(lambda restore-vert-split [direction bufnr]
+  "Opens bottom split terminal at 33% height."
+  (print "Restored existing split.")
+  (let [bufheight (f.floor (/ (f.winwidth 0) 3))]
+    (vim.cmd (.. direction " " bufheight "vsplit"))
+    (api.nvim_set_current_buf bufnr)))
 
 (lambda get-line-at-cursor []
   (->
@@ -41,13 +49,100 @@
     (#(api.nvim_buf_get_lines 0 (- $1 1) $1 true))
     (#(. $1 1))))
 
-(lambda yank-line-to-register []
-  (local line (get-line-at-cursor))
+(lambda get-index [table value]
+  (var index nil)
+  (each [t-ind t-val (pairs table)]
+    (if (= t-val value)
+      (set index t-ind)))
+  index)
 
-  (if (> (length line) 3)
-    (let [letter (string.sub line 1 1)
-          contents (string.sub line 4)]
-      (f.setreg letter contents)
-      (print (.. "Yanked to register " letter)))))
+(lambda add-pid [status-extmark-id pid]
+  (tset pids status-extmark-id pid))
 
-{:run run :display_registers display-registers :yank_line_to_register yank-line-to-register}
+(lambda remove-pid [pid]
+  (let [index (get-index pids pid)]
+    (tset pids index nil)))
+
+(lambda get-pid-by-status-extmark [status-extmark-id]
+  (. pids status-extmark-id))
+
+(lambda get-extmarks-at-line []
+  (let [line-nr (. (api.nvim_win_get_cursor 0) 1)]
+    (api.nvim_buf_get_extmarks 0 ns [(- line-nr 1) 0] [(- line-nr 1) 0] [])))
+
+(lambda clear-extmarks-at-line []
+  (let [extmarks (get-extmarks-at-line)]
+    (each [_ [extmark-id _ _] (ipairs extmarks)]
+      (api.nvim_buf_del_extmark 0 ns extmark-id))))
+
+(lambda create-extmark-at-line []
+  "Creates extmark and returns its id"
+  (api.nvim_buf_set_extmark 0 ns (- (. (api.nvim_win_get_cursor 0) 1) 1) 0 {}))
+
+; {row col {:hl_eol true ... virt_lines [[["text" "Comment"]]]}}
+; https://github.com/neovim/neovim/pull/17076
+(lambda get-lines-at-extmark [bufnr extmark-id]
+  (or (-> (api.nvim_buf_get_extmark_by_id bufnr ns extmark-id {:details true})
+          (. 3)
+          (. :virt_lines)
+          )
+      []))
+
+(lambda get-row-at-extmark [bufnr extmark-id]
+  (-> (api.nvim_buf_get_extmark_by_id bufnr ns extmark-id {:details true})
+      (. 1)))
+
+(lambda set-lines-at-extmark [bufnr extmark-id lines]
+  (let [line-nr (get-row-at-extmark bufnr extmark-id)]
+    (api.nvim_buf_set_extmark bufnr ns line-nr 0 {:id extmark-id
+                                                  :virt_lines lines})))
+
+(lambda append-lines-to-extmark [bufnr extmark-id newline hl]
+  "Appends line to extmark"
+  (let [lines (get-lines-at-extmark bufnr extmark-id)]
+    (table.insert lines [[newline hl]])
+    (set-lines-at-extmark bufnr extmark-id lines)))
+
+(lambda set-extmark [bufnr extmark-id text hl]
+  (let [line-nr (get-row-at-extmark bufnr extmark-id)]
+    (api.nvim_buf_set_extmark bufnr ns line-nr 0 {:id extmark-id
+                                                  :virt_text [[text hl]]})))
+
+(lambda execute-line []
+  (clear-extmarks-at-line)
+  (let [line (get-line-at-cursor)
+        output-extmark-id (create-extmark-at-line)
+        status-extmark-id (create-extmark-at-line)
+        bufnr (f.bufnr)
+        job (Job:new {:command "/bin/sh"
+                      :args ["-c" line]
+                      :on_stdout (vim.schedule_wrap (fn [_ data] (append-lines-to-extmark bufnr output-extmark-id data "Comment")))
+                      :on_stderr (vim.schedule_wrap (fn [_ data] (append-lines-to-extmark bufnr output-extmark-id data "Error")))
+                      :on_exit (vim.schedule_wrap (lambda [j retval] (do
+                                                                       (remove-pid j.pid)
+                                                                       (set-extmark bufnr status-extmark-id "■"
+                                                                                    (if (or (~= 0 j.code) (~= 0 j.signal))
+                                                                                      "DiagnosticVirtualTextError"
+                                                                                      "DiagnosticVirtualTextHint")))))})]
+    ; TODO: set start and end times
+    (set-extmark bufnr status-extmark-id "■" "DiagnosticVirtualTextWarn")
+    (: job "start")
+    (add-pid status-extmark-id job.pid)))
+
+(lambda stop-execution-at-line [signal]
+  (each [_ [extmark-id _ _] (ipairs (get-extmarks-at-line))]
+    (let [pid (get-pid-by-status-extmark extmark-id)]
+      (if (~= nil pid)
+        (kill-pid pid signal)))))
+
+(lambda open-tags []
+  (if (or (= tagbufnr nil) (not (f.bufexists tagbufnr)))
+    (do (->> (open-vert-split "belowright")
+             (set tagbufnr))
+      (cmd (.. "edit .tagbar"))
+      (vim.keymap.set "n" "<CR>" execute-line {:buffer tagbufnr})
+      (vim.keymap.set "n" "\\" (fn [] (stop-execution-at-line 15) {:buffer tagbufnr}))
+      (vim.keymap.set "n" "<C-\\>" (fn [] (stop-execution-at-line 9) {:buffer tagbufnr})))
+    (restore-vert-split "belowright" tagbufnr)))
+
+{:open-tags open-tags}
--- lua/acmetag.lua
+++ lua/acmetag.lua
@@ -1,76 +1,194 @@
 local f = vim.fn
 local api = vim.api
-local function open_split(direction)
-  _G.assert((nil ~= direction), "Missing argument direction on fnl/acmetag.fnl:4")
-  local bufheight = f.floor((f.winheight(0) / 5))
-  vim.cmd((direction .. " " .. bufheight .. "split"))
+local cmd = vim.cmd
+local Job = require("plenary.job")
+local tagbufnr = nil
+local pids = {}
+local ns = api.nvim_create_namespace("acmetag")
+local function kill_pid(pid, killcode_3f)
+  return io.popen(("kill -" .. (killcode_3f or 9) .. " " .. pid))
+end
+local function kill_all_running_processes()
+  for _, pid in pairs(pids) do
+    kill_pid(pid)
+  end
+  return nil
+end
+api.nvim_create_autocmd("VimLeavePre", {callback = kill_all_running_processes})
+local function open_vert_split(direction)
+  _G.assert((nil ~= direction), "Missing argument direction on fnl/acmetag.fnl:31")
+  local bufheight = f.floor((f.winwidth(0) / 3))
+  vim.cmd((direction .. " " .. bufheight .. "vsplit"))
   local bufnr = api.nvim_create_buf(false, false)
   api.nvim_set_current_buf(bufnr)
   return bufnr
 end
-local function run(letter)
-  _G.assert((nil ~= letter), "Missing argument letter on fnl/acmetag.fnl:12")
-  local command = string.match(f.getreg(letter), "^%s*(.-)%s*$")
-  local bufheight = f.floor((f.winheight(0) / 5))
-  if (command == "") then
-    return print(("nothing bound to register '" .. letter .. "' !"))
-  else
-    open_split("belowright")
-    f.termopen(command)
-    return api.nvim_command("startinsert")
-  end
-end
-local letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-local function display_registers()
-  local bufnr = open_split("topleft")
-  local function _2_()
-    local tbl_15_auto = {}
-    local i_16_auto = #tbl_15_auto
-    for _, letter in ipairs(letters) do
-      local val_17_auto
-      do
-        local reg = f.getreg(letter, 1)
-        if not string.find(reg, "\n") then
-          val_17_auto = (letter .. ": " .. reg)
-        else
-          val_17_auto = nil
-        end
-      end
-      if (nil ~= val_17_auto) then
-        i_16_auto = (i_16_auto + 1)
-        do end (tbl_15_auto)[i_16_auto] = val_17_auto
-      else
-      end
-    end
-    return tbl_15_auto
-  end
-  api.nvim_buf_set_lines(bufnr, 0, -1, true, _2_())
-  vim.bo.bufhidden = "hide"
-  vim.bo.buflisted = false
-  vim.bo.buftype = "nofile"
-  return nil
+local function restore_vert_split(direction, bufnr)
+  _G.assert((nil ~= bufnr), "Missing argument bufnr on fnl/acmetag.fnl:39")
+  _G.assert((nil ~= direction), "Missing argument direction on fnl/acmetag.fnl:39")
+  print("Restored existing split.")
+  local bufheight = f.floor((f.winwidth(0) / 3))
+  vim.cmd((direction .. " " .. bufheight .. "vsplit"))
+  return api.nvim_set_current_buf(bufnr)
 end
 local function get_line_at_cursor()
-  local function _5_(_241)
+  local function _1_(_241)
     return (_241)[1]
   end
-  local function _6_(_241)
+  local function _2_(_241)
     return api.nvim_buf_get_lines(0, (_241 - 1), _241, true)
   end
-  local function _7_(_241)
+  local function _3_(_241)
     return (_241)[1]
   end
-  return _5_(_6_(_7_(api.nvim_win_get_cursor(0))))
+  return _1_(_2_(_3_(api.nvim_win_get_cursor(0))))
 end
-local function yank_line_to_register()
+local function get_index(table, value)
+  _G.assert((nil ~= value), "Missing argument value on fnl/acmetag.fnl:53")
+  _G.assert((nil ~= table), "Missing argument table on fnl/acmetag.fnl:53")
+  local index = nil
+  for t_ind, t_val in pairs(table) do
+    if (t_val == value) then
+      index = t_ind
+    else
+    end
+  end
+  return index
+end
+local function add_pid(status_extmark_id, pid)
+  _G.assert((nil ~= pid), "Missing argument pid on fnl/acmetag.fnl:60")
+  _G.assert((nil ~= status_extmark_id), "Missing argument status-extmark-id on fnl/acmetag.fnl:60")
+  do end (pids)[status_extmark_id] = pid
+  return nil
+end
+local function remove_pid(pid)
+  _G.assert((nil ~= pid), "Missing argument pid on fnl/acmetag.fnl:63")
+  local index = get_index(pids, pid)
+  do end (pids)[index] = nil
+  return nil
+end
+local function get_pid_by_status_extmark(status_extmark_id)
+  _G.assert((nil ~= status_extmark_id), "Missing argument status-extmark-id on fnl/acmetag.fnl:67")
+  return pids[status_extmark_id]
+end
+local function get_extmarks_at_line()
+  local line_nr = (api.nvim_win_get_cursor(0))[1]
+  return api.nvim_buf_get_extmarks(0, ns, {(line_nr - 1), 0}, {(line_nr - 1), 0}, {})
+end
+local function clear_extmarks_at_line()
+  local extmarks = get_extmarks_at_line()
+  for _, _5_ in ipairs(extmarks) do
+    local _each_6_ = _5_
+    local extmark_id = _each_6_[1]
+    local _0 = _each_6_[2]
+    local _1 = _each_6_[3]
+    api.nvim_buf_del_extmark(0, ns, extmark_id)
+  end
+  return nil
+end
+local function create_extmark_at_line()
+  return api.nvim_buf_set_extmark(0, ns, ((api.nvim_win_get_cursor(0))[1] - 1), 0, {})
+end
+local function get_lines_at_extmark(bufnr, extmark_id)
+  _G.assert((nil ~= extmark_id), "Missing argument extmark-id on fnl/acmetag.fnl:85")
+  _G.assert((nil ~= bufnr), "Missing argument bufnr on fnl/acmetag.fnl:85")
+  local function _7_()
+    return ((api.nvim_buf_get_extmark_by_id(bufnr, ns, extmark_id, {details = true}))[3]).virt_lines
+  end
+  return (_7_() or {})
+end
+local function get_row_at_extmark(bufnr, extmark_id)
+  _G.assert((nil ~= extmark_id), "Missing argument extmark-id on fnl/acmetag.fnl:92")
+  _G.assert((nil ~= bufnr), "Missing argument bufnr on fnl/acmetag.fnl:92")
+  return (api.nvim_buf_get_extmark_by_id(bufnr, ns, extmark_id, {details = true}))[1]
+end
+local function set_lines_at_extmark(bufnr, extmark_id, lines)
+  _G.assert((nil ~= lines), "Missing argument lines on fnl/acmetag.fnl:96")
+  _G.assert((nil ~= extmark_id), "Missing argument extmark-id on fnl/acmetag.fnl:96")
+  _G.assert((nil ~= bufnr), "Missing argument bufnr on fnl/acmetag.fnl:96")
+  local line_nr = get_row_at_extmark(bufnr, extmark_id)
+  return api.nvim_buf_set_extmark(bufnr, ns, line_nr, 0, {id = extmark_id, virt_lines = lines})
+end
+local function append_lines_to_extmark(bufnr, extmark_id, newline, hl)
+  _G.assert((nil ~= hl), "Missing argument hl on fnl/acmetag.fnl:101")
+  _G.assert((nil ~= newline), "Missing argument newline on fnl/acmetag.fnl:101")
+  _G.assert((nil ~= extmark_id), "Missing argument extmark-id on fnl/acmetag.fnl:101")
+  _G.assert((nil ~= bufnr), "Missing argument bufnr on fnl/acmetag.fnl:101")
+  local lines = get_lines_at_extmark(bufnr, extmark_id)
+  table.insert(lines, {{newline, hl}})
+  return set_lines_at_extmark(bufnr, extmark_id, lines)
+end
+local function set_extmark(bufnr, extmark_id, text, hl)
+  _G.assert((nil ~= hl), "Missing argument hl on fnl/acmetag.fnl:107")
+  _G.assert((nil ~= text), "Missing argument text on fnl/acmetag.fnl:107")
+  _G.assert((nil ~= extmark_id), "Missing argument extmark-id on fnl/acmetag.fnl:107")
+  _G.assert((nil ~= bufnr), "Missing argument bufnr on fnl/acmetag.fnl:107")
+  local line_nr = get_row_at_extmark(bufnr, extmark_id)
+  return api.nvim_buf_set_extmark(bufnr, ns, line_nr, 0, {id = extmark_id, virt_text = {{text, hl}}})
+end
+local function execute_line()
+  clear_extmarks_at_line()
   local line = get_line_at_cursor()
-  if (#line > 3) then
-    local letter = string.sub(line, 1, 1)
-    local contents = string.sub(line, 4)
-    f.setreg(letter, contents)
-    return print(("Yanked to register " .. letter))
+  local output_extmark_id = create_extmark_at_line()
+  local status_extmark_id = create_extmark_at_line()
+  local bufnr = f.bufnr()
+  local job
+  local function _8_(_, data)
+    return append_lines_to_extmark(bufnr, output_extmark_id, data, "Comment")
+  end
+  local function _9_(_, data)
+    return append_lines_to_extmark(bufnr, output_extmark_id, data, "Error")
+  end
+  local function _10_(j, retval)
+    _G.assert((nil ~= retval), "Missing argument retval on fnl/acmetag.fnl:122")
+    _G.assert((nil ~= j), "Missing argument j on fnl/acmetag.fnl:122")
+    remove_pid(j.pid)
+    local function _11_()
+      if ((0 ~= j.code) or (0 ~= j.signal)) then
+        return "DiagnosticVirtualTextError"
+      else
+        return "DiagnosticVirtualTextHint"
+      end
+    end
+    return set_extmark(bufnr, status_extmark_id, "\226\150\160", _11_())
+  end
+  job = Job:new({command = "/bin/sh", args = {"-c", line}, on_stdout = vim.schedule_wrap(_8_), on_stderr = vim.schedule_wrap(_9_), on_exit = vim.schedule_wrap(_10_)})
+  set_extmark(bufnr, status_extmark_id, "\226\150\160", "DiagnosticVirtualTextWarn")
+  job:start()
+  return add_pid(status_extmark_id, job.pid)
+end
+local function stop_execution_at_line(signal)
+  _G.assert((nil ~= signal), "Missing argument signal on fnl/acmetag.fnl:133")
+  for _, _12_ in ipairs(get_extmarks_at_line()) do
+    local _each_13_ = _12_
+    local extmark_id = _each_13_[1]
+    local _0 = _each_13_[2]
+    local _1 = _each_13_[3]
+    local pid = get_pid_by_status_extmark(extmark_id)
+    if (nil ~= pid) then
+      kill_pid(pid, signal)
+    else
+    end
+  end
+  return nil
+end
+local function open_tags()
+  if ((tagbufnr == nil) or not f.bufexists(tagbufnr)) then
+    tagbufnr = open_vert_split("belowright")
+    cmd("edit .tagbar")
+    vim.keymap.set("n", "<CR>", execute_line, {buffer = tagbufnr})
+    local function _15_()
+      stop_execution_at_line(15)
+      return {buffer = tagbufnr}
+    end
+    vim.keymap.set("n", "\\", _15_)
+    local function _16_()
+      stop_execution_at_line(9)
+      return {buffer = tagbufnr}
+    end
+    return vim.keymap.set("n", "<C-\\>", _16_)
   else
-    return nil
+    return restore_vert_split("belowright", tagbufnr)
   end
 end
-return {run = run, display_registers = display_registers, yank_line_to_register = yank_line_to_register}
+return {["open-tags"] = open_tags}
